010235b157cbe312d336f3ae97b41ed8ad69628c
emmajane1313 <admin@digitalax.xyz>
2022-10-30 12:53:57 -0400

    opacity footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,8 +3,8 @@ import { FunctionComponent } from "react";
 
 const Footer: FunctionComponent = (): JSX.Element => {
   return (
-    <div className="bg-offBlack w-full h-40 relative grid grid-flow-col auto-cols-[auto auto]">
-      <div className="relative col-start-1 justify-self-end self-center pr-6 w-fit h-fit">
+    <div className="bg-offBlack w-full h-36 opacity-70 relative grid grid-flow-col auto-cols-[auto auto]">
+      <div className="relative col-start-1 opacity-100 justify-self-end self-center pr-6 w-fit h-fit">
         <Image
           width={120}
           height={70}
